c4fdecad04252955450287b8c62ea6d3947c6223
oma710 <hamza.omaddi@gmail.com>
2024-01-24 18:40:42 +0100

    added compte DTO

--- src/main/java/org/sid/dto/CompteDTO.java
+++ src/main/java/org/sid/dto/CompteDTO.java
@@ -1,12 +1,16 @@
 package org.sid.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 public abstract class CompteDTO {
     private String codeCompte;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private Date dateCreation;
     private double solde;
     private Long clientId;
