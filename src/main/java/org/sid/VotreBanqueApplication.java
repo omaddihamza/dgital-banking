967f45d1cd2409e90e2e869841fcb131495e6018
oma710 <hamza.omaddi@gmail.com>
2024-01-24 18:44:58 +0100

    added modelMapper

--- src/main/java/org/sid/VotreBanqueApplication.java
+++ src/main/java/org/sid/VotreBanqueApplication.java
@@ -1,11 +1,18 @@
 package org.sid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 
 public class VotreBanqueApplication {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
     public static void main(String[] args) {
         SpringApplication.run(VotreBanqueApplication.class, args);
     }
