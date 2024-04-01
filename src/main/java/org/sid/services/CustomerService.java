27bfb45da508f8f204ea3598af7642501dd254bc
oma710 <hamza.omaddi@gmail.com>
2024-04-01 23:55:09 +0000

    add method on service and implement them

--- src/main/java/org/sid/services/CustomerService.java
+++ src/main/java/org/sid/services/CustomerService.java
@@ -1,5 +1,11 @@
 package org.sid.services;
 
-public interface CustomerService {
+import org.sid.dto.CustomerDTO;
+import org.sid.entities.Customer;
 
+public interface CustomerService {
+    Customer createCustomer(CustomerDTO customerDTO);
+    Customer getCustomerById(Long userId);
+    Customer updateCustomer(Long customerId, CustomerDTO customerDTO);
+    void deleteCustomerId(Long customerId);
 }
--- src/main/java/org/sid/services/implement/CustomerImpl.java
+++ src/main/java/org/sid/services/implement/CustomerImpl.java
@@ -1,4 +1,48 @@
 package org.sid.services.implement;
 
-public class CustomerImpl {
+import org.modelmapper.ModelMapper;
+import org.sid.Exception.CustomerNotFoundException;
+import org.sid.dto.CustomerDTO;
+import org.sid.entities.Customer;
+import org.sid.repository.CustomerRepository;
+import org.sid.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public Customer createCustomer(CustomerDTO customerDTO) {
+      Customer customer = modelMapper.map(customerDTO, Customer.class);
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer getCustomerById(Long customerId) {
+        return customerRepository.findById(customerId).orElseThrow(()-> new CustomerNotFoundException("Customer not found with ID: " + customerId));
+    }
+
+    @Override
+    public Customer updateCustomer(Long customerId, CustomerDTO customerDTO) {
+        Customer existingCustomer = customerRepository.findById(customerId).orElseThrow(()-> new CustomerNotFoundException("Customer not found with ID: " + customerId));
+        existingCustomer.setUsername(customerDTO.getUsername());
+        existingCustomer.setPassword(customerDTO.getPassword());
+        existingCustomer.setEmail(customerDTO.getEmail());
+        existingCustomer.setFirst_name(customerDTO.getFirst_name());
+        existingCustomer.setLast_name(customerDTO.getLast_name());
+        existingCustomer.setDate_of_birth(customerDTO.getDate_of_birth());
+        existingCustomer.setAddress(customerDTO.getAddress());
+        existingCustomer.setPhone_number(customerDTO.getPhone_number());
+        return customerRepository.save(existingCustomer);
+    }
+
+    @Override
+    public void deleteCustomerId(Long customerId) {
+      customerRepository.deleteById(customerId);
+    }
 }
