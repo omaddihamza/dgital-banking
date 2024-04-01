9026766447c9ef28d24c671308c5c605363524ca
oma710 <hamza.omaddi@gmail.com>
2024-04-01 23:56:21 +0000

    add Temporal annotation

--- src/main/java/org/sid/entities/Customer.java
+++ src/main/java/org/sid/entities/Customer.java
@@ -1,6 +1,7 @@
 package org.sid.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.Date;
@@ -10,13 +11,16 @@ import java.util.Date;
 @NoArgsConstructor
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
     private String username;
+    @Size(min = 8, message = "Password must be at least 6 characters long")
     private String password;
     private String email;
     private String first_name;
     private String last_name;
+    @Temporal(TemporalType.DATE)
     private Date date_of_birth;
     @Embedded
     private Address address;
